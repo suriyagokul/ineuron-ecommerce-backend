696f921fec89f0a17b7a54dd40bbbcb792270235
suriyagokul <124340052+suriyagokul@users.noreply.github.com>
2023-06-24 04:37:08 +0000

    added product controllers

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,7 +3,7 @@ import asyncHandler from "../service/asyncHandler.js"
 import CustomError from "../utils/CustomError.js"
 import config from "../config"
 import Mongoose from "mongoose"
-import {s3FileUpload} from "../service/imageUpload.js"
+import {s3FileUpload, s3deleteFile} from "../service/imageUpload.js"
 import formidable from "formidable"
 import fs from "fs"
 
@@ -39,7 +39,7 @@ export const addProduct = asyncHandler(async (req, res) => {
                 console.log(upload);
 
                 return {
-                    secure_url: upload.Location;
+                    secure_url: upload.Location,
                 }
 
             })
@@ -58,9 +58,91 @@ export const addProduct = asyncHandler(async (req, res) => {
         }
 
         res.status(200).json({
-            success: true;
+            success: true,
             product
         })
 
+    })
+})
+
+export const getAllProducts = asyncHandler(async (req, res) => {
+   
+    const products = await Product.find({})
+
+    if(!products){
+        throw new CustomError("No Products Found", 404)
+    }
+
+    res.status(200).json({
+        success: true,
+        products
+    })
+})
+
+export const getProductById = asyncHandler(async (req, res) => {
+
+    const {id: productId} = req.params
+
+    const product = await Product.findById(productId);
+
+    if(!product){
+        throw new CustomError("No Product Found", 404)
+    }
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+})
+
+export const getProductByCollectionId = asyncHandler(async (req, res)=> {
+    const {id: collectionId} = req.params;
+
+    const products = await Product.find({collectionId})
+
+    if(!products){
+        throw new CustomError("No Products Found", 404)
+    }
+
+    res.status(200).json({
+        success: true,
+        products
+    })
+})
+
+export const deleteProduct = asyncHandler(async (req, res)=> {
+    const {id: productId} = req.params;
+
+    const productToDelete = await Product.findById(productId)
+
+    if(!productToDelete){
+        throw new CustomError("No Product Found To Delete", 400)
+    }
+
+    /* steps to delete photos of this corresponding product
+    
+            1) Resolve Promise
+
+            2) Loop Through this product photos
+
+            3) Delete photos from aws 
+    */
+
+    const deletePhotos = Promise.all(
+        productToDelete.photos.map(async (elem, index)=> {
+            await s3deleteFile({
+                bucketName: config.S3_BUCKET_NAME,
+                key: `products/${productToDelete._id}/photo_${index + 1}.png`
+            })
+        })
+    )
+
+    await deletePhotos;
+
+    await productToDelete.remove() 
+
+    res.status(200).json({
+        succes: true,
+        message: 'Product has been deleted successfully'
     })
 })
