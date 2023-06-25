0cbcc267cdcb48abb105d4287502a1dd3ac50b27
suriyagokul <124340052+suriyagokul@users.noreply.github.com>
2023-06-25 05:50:09 +0000

    added update and delete coupon controllers and coupon routes

---
+++ src/routes/coupon.route.js
@@ -0,0 +1,16 @@
+import {Router} from "express"
+import {isLoggedIn, authorize} from "../middlewares/auth.middleware"
+import AuthRoles from "../utils/authRoles.js"
+import {createCoupon, updateCoupon, deleteCoupon, getAllCoupons} from "../controllers/coupon.controller"
+
+const router = Router()
+
+router.post("/", isLoggedIn, authorize(AuthRoles.ADMIN, AuthRoles.MODERATOR), createCoupon)
+router.put("/action/:id", isLoggedIn, authorize(AuthRoles.ADMIN), updateCoupon)
+router.delete("/:id", isLoggedIn, authorize(AuthRoles.ADMIN), deleteCoupon)
+router.get("/", isLoggedIn, authorize(AuthRoles.ADMIN, AuthRoles.MODERATOR), getAllCoupons)
+
+
+
+
+export default router
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 import {Router} from "express"
 import authRoutes from "./auth.route.js"
+import couponRoutes from "./coupon.route.js"
 
 
 const router = Router()
 router.use("/auth", authRoutes)
+router.use("/coupon", couponRoutes)
+
 
 
 export default router;
