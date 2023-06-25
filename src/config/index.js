c553bccf6c11c193d6f7b6e9c447b19e212684a5
suriyagokul <124340052+suriyagokul@users.noreply.github.com>
2023-06-25 13:57:31 +0000

    added razorpay

--- src/config/index.js
+++ src/config/index.js
@@ -17,7 +17,10 @@ const config = {
   SMTP_MAIL_PORT: process.env.SMTP_MAIL_PORT,
   SMTP_MAIL_USERNAME: process.env.SMTP_MAIL_USERNAME,
   SMTP_MAIL_PASSWORD: process.env.SMTP_MAIL_PASSWORD,
-  SMTP_SENDER_EMAIL: process.env.SMTP_SENDER_EMAIL
+  SMTP_SENDER_EMAIL: process.env.SMTP_SENDER_EMAIL,
+
+  RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID,
+  RAZORPAY_SECRET: process.env.RAZORPAY_SECRET
 };
 
 export default config;
