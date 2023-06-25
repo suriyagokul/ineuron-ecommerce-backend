cd745ca1bdf7f7e1dff2333ada1def419ce939ff
suriyagokul <124340052+suriyagokul@users.noreply.github.com>
2023-06-25 05:16:27 +0000

    forgotpassword and resetpassword controller methods added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.schema.js"
 import asyncHandler from "../service/asyncHandler.js";
 import CustomError from "../utils/CustomError.js";
+import mailHelper from "../utils/mailHelper.js"
+import crypto from "crypto"
+
 
 export const cookieOptions = {
    expires: new Date(Date.now() + 3*24*60*60*1000),  // 3 days
@@ -97,4 +100,76 @@ export const getProfile = asyncHandler(async (req, res)=> {
       user
    })
 
+})
+
+export const forgotPassword = asyncHandler(async (req, res)=> {
+   const {email} = req.body;
+
+   if(!email){
+      throw new CustomError("Please Provide Email",400)
+   }
+
+   const user = User.findOne({email})
+
+   if(!user){
+      throw new CustomError("No user found with this email",404)
+   }
+
+   const resetToken = user.generateforgotPasswordToken()
+
+   await user.save({validateBeforeSave: false})
+
+   const tokenUrl = `${req.protocol}://${req.get("host")}/api/v1/auth/password/reset/${resetToken}`;
+
+   const message  = `Please reset your password using below link \n\n ${tokenUrl} \n\n if this was not requested by you, please ignore.`
+
+  try {
+      await mailHelper({
+         email: user.email,
+         subject: "Password Reset Token",
+         message 
+      })
+  } catch (error) {
+      user.forgotPasswordToken = undefined,
+      user.forgotPasswordExpiry = undefined
+
+      await user.save({validateBeforeSave: false})
+
+      throw new CustomError(error.message || "Email could not be sent", 500)
+  }
+})
+
+export const resetPassword = asyncHandler(async (req, res)=> {
+   const {token: resetToken} = req.params;
+   const {password, confirmPassword} = req.body;
+
+   const resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex")
+
+   const user = User.findOne({
+      forgotPasswordToken: resetPasswordToken,
+      forgotPasswordExpiry: { $gt: Date.now() }
+   })
+
+   if(!user){
+      throw new CustomError("passowrd token is invalid or expired",400)
+   }
+
+   if(password !== confirmPassword){
+      throw new CustomError("Password doesn't match",401)
+   }
+
+   user.password = password
+   user.forgotPasswordToken = undefined
+   user.forgotPasswordExpiry = undefined
+
+   await user.save();
+
+   const token = user.getJWTtoken()
+
+   res.cookie("token", token, cookieOptions);
+
+   res.status(200).json({
+      success: true,
+      user
+   })
 })
--- src/utils/mailHelper.js
+++ src/utils/mailHelper.js
@@ -1,3 +1,15 @@
 import nodemailer from "nodemailer"
+import transporter from "../config/transporter.config.js"
+import config from "../config/index.js"
 
-nodemailer.
+const mailHelper = async (option) => {
+    const message = {
+        from: config.SMTP_SENDER_EMAIL,
+        to: option.email,
+        subject: option.subject,
+        text: option.message
+    }
+    await transporter.sendMail(message)
+}
+
+export default mailHelper
